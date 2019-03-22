691d17341237db9fb38d8701cd5a93e102cfa8f5
AndreaBonafini <47528585+AndreaBonafini@users.noreply.github.com>
2019-03-22 10:05:53 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fibonacci_sequence_drawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int s1 = 0;
+            int s2 = 1;
+            int numberofItem = 2;
+            int.TryParse(txtNumber.Text, out numberofItem);
+            string fib = "";
+            if (numberofItem >= 1)
+            {
+                fib = s1.ToString();
+            }
+            if (numberofItem >= 1)
+            {
+                fib += "," + s2.ToString();
+            }
+            for (int i = 2; i < numberofItem; i++)
+            {
+                int next = s1 + s2;
+                fib += "," + next.ToString();
+                s1 = s2;
+                s2 = next;
+            }
+            MessageBox.Show(fib);
+        }
+       
+    }
+}
+
